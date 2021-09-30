c112a6d5fa4174ad8b8d6cfda49fd86340c92a76
이지원 <jwlee@ijiwon-ui-MacBookPro.local>
2021-10-01 07:22:40 +0900

    2.2.2 회원가입 뷰에 씌우기

--- lib/view/pages/user/join_page.dart
+++ lib/view/pages/user/join_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/controller/user_controller.dart';
 
 import 'package:flutter_blog/util/validator_util.dart';
 import 'package:flutter_blog/view/components/custom_elevated_button.dart';
 import 'package:flutter_blog/view/components/custom_text_form_field.dart';
+import 'package:flutter_blog/view/pages/post/home_page.dart';
 import 'package:get/get.dart';
+
+import 'login_page.dart';
 
 class JoinPage extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
-  final _email = TextEditingController();
-  final _password = TextEditingController();
+  final UserController u = Get.put(UserController());
+
   final _username = TextEditingController();
+  final _password = TextEditingController();
+  final _email = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,25 +49,46 @@ class JoinPage extends StatelessWidget {
       child: Column(
         children: [
           CustomTextFormField(
-            controller: _email,
             hint: "Email",
+            controller: _email,
             funValidator: validateEmail(),
           ),
           CustomTextFormField(
-            controller: _password,
             hint: "Password",
+            controller: _password,
             funValidator: validatePassword(),
           ),
           CustomTextFormField(
-            controller: _username,
             hint: "Username",
+            controller: _username,
             funValidator: validateUsername(),
           ),
           CustomElevatedButton(
             text: "회원가입",
-            funPageRoute: () {
+            funPageRoute: () async {
               if (_formKey.currentState!.validate()) {
-                //
+                int emailCheck = await u.checkEmail(_email.text.trim());
+                if (emailCheck < 1) {
+                  Get.snackbar("회원가입 실패", "이메일 중복");
+                  return;
+                }
+                int usernameCheck =
+                    await u.checkUsername(_username.text.trim());
+                if (usernameCheck < 1) {
+                  Get.snackbar("회원가입 실패", "유저네임 중복");
+                  return;
+                }
+                int result = await u.join(
+                  _email.text.trim(),
+                  _password.text.trim(),
+                  _username.text.trim(),
+                );
+
+                if (result == 1) {
+                  Get.offAll(() => HomePage());
+                } else {
+                  Get.snackbar("회원가입 시도", "회원가입 실패");
+                }
               }
             },
           ),
